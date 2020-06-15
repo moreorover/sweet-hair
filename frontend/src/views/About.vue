d753c9242d4eae3dc687fff0bc834ea0886408cf
Martin <mselvenis@gmail.com>
2020-06-16 00:37:23 +0100

    About page as temporary dashboard

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-row>
+      <v-col cols="12" md="4">
+        <OrdersList/>
+      </v-col>
+      <v-col cols="12" md="4">
+        <SuppliersList/>
+      </v-col>
+      <v-col cols="12" md="4">
+        <ProductsList/>
+      </v-col>
+    </v-row>
   </div>
 </template>
+<script>
+  import OrdersList from "./OrdersList";
+  import SuppliersList from "./SuppliersList";
+  import ProductsList from "./ProductsList";
+
+  export default {
+    name: "About",
+    components: {
+      OrdersList, ProductsList, SuppliersList
+    }
+  }
+</script>
