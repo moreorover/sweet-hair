0d86fcc830f6fd0a010f3610030655a9c01245bb
Martin <mselvenis@gmail.com>
2020-06-16 00:58:22 +0100

    4 columns at xl screen size

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -2,14 +2,17 @@
   <div class="about">
     <h1>This is an about page</h1>
     <v-row>
-      <v-col cols="12" md="4">
+      <v-col cols="12" md="4" xl="3">
         <OrdersList/>
       </v-col>
-      <v-col cols="12" md="4">
+      <v-col cols="12" md="4" xl="3">
         <SuppliersList/>
       </v-col>
-      <v-col cols="12" md="4">
+      <v-col cols="12" md="4" xl="3">
         <ProductsList/>
+      </v-col>
+      <v-col cols="12" md="4" xl="3">
+        4th column
       </v-col>
     </v-row>
   </div>
