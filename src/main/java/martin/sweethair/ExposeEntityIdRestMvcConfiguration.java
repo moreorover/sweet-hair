db5418e2a3d574380b23b5aaa79df5eeaa291275
Martin <mselvenis@gmail.com>
2020-06-16 10:49:45 +0100

    Exposing ID's on API endpoints

--- src/main/java/martin/sweethair/ExposeEntityIdRestMvcConfiguration.java
+++ src/main/java/martin/sweethair/ExposeEntityIdRestMvcConfiguration.java
@@ -1,7 +1,6 @@
 package martin.sweethair;
 
-import martin.sweethair.data.models.Product;
-import martin.sweethair.data.models.Supplier;
+import martin.sweethair.data.models.*;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.stereotype.Component;
@@ -11,7 +10,12 @@ public class ExposeEntityIdRestMvcConfiguration implements RepositoryRestConfigu
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration restConfig) {
+        restConfig.exposeIdsFor(Customer.class);
+        restConfig.exposeIdsFor(Order.class);
+        restConfig.exposeIdsFor(OrderItems.class);
         restConfig.exposeIdsFor(Product.class);
+        restConfig.exposeIdsFor(Sale.class);
+        restConfig.exposeIdsFor(SaleItems.class);
         restConfig.exposeIdsFor(Supplier.class);
     }
 }
