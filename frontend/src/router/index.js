e5cf2352e9e10777c837a039a131c012687a1859
Martin <mselvenis@gmail.com>
2020-06-23 09:13:48 +0100

    Product routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -29,15 +29,6 @@ Vue.use(VueRouter)
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/OrdersList'),
     props: true
-  },
-  {
-    path: '/products',
-    name: 'Products',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/ProductsList'),
-    props: true
   }
 ]
 
