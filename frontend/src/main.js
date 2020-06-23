bc9cbdb0a0d6aaca4e7947bea31f4187ee03368f
Martin <mselvenis@gmail.com>
2020-06-23 07:45:48 +0100

    Lodash dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import vuetify from './plugins/vuetify';
+import lodash from "./plugins/lodash";
 import 'nprogress/nprogress.css'
 
 axios.defaults.baseURL = "http://localhost:8080/api"
@@ -14,5 +15,6 @@ new Vue({
   router,
   store,
   vuetify,
+  lodash,
   render: h => h(App)
 }).$mount('#app')
