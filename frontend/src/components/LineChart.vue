2b1d8cdff6f6d6b4a0bc5a3e2c699e9293596d7d
Martin <mselvenis@gmail.com>
2020-07-02 22:48:57 +0100

    base Line Chart dependency

---
+++ frontend/src/components/LineChart.vue
@@ -0,0 +1,38 @@
+<script>
+    import {Line} from "vue-chartjs";
+
+    export default {
+        extends: Line,
+        name: "LineChart",
+        props: {
+            label: {
+                type: String,
+                required: true
+            },
+            labels: {
+                type: Array,
+                required: false
+            },
+            values: {
+                type: Array,
+                required: true
+            }
+        },
+        mounted () {
+            this.renderChart({
+                labels: this.labels,
+                datasets: [
+                    {
+                        label: this.label,
+                        backgroundColor: '#f87979',
+                        data: this.values
+                    }
+                ]
+            }, {responsive: true, maintainAspectRatio: false})
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
